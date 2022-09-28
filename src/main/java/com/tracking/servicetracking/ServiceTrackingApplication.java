471c81990822a8f1160aa1ab823535c093462e00
sagar9512 <sagar.pawar6313@gmail.com>
2022-09-28 15:53:33 +0530

    Added project structure

---
+++ src/main/java/com/tracking/servicetracking/ServiceTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.tracking.servicetracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracking/servicetracking/ServiceTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracking.servicetracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
